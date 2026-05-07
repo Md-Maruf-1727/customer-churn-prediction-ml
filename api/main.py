bb84ea62b85dee0445722a52b272da4bcab4990c
Md-Maruf-1727 <maruf.md1727@gmail.com>
2026-05-07 21:32:14 +0600

    error fixing

--- api/main.py
+++ api/main.py
@@ -1,50 +1,56 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 import joblib
 
-from api.preprocess import preprocess 
+from api.preprocess import preprocess
 from utils.path import get_model_path
 from api.schema import ChurnInput
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 model = joblib.load(get_model_path("xgb_model.joblib"))
 scaler = joblib.load(get_model_path("scaler.joblib"))
 feature_columns = joblib.load(get_model_path("feature_columns.joblib"))
+
 
 @app.get("/")
 def home():
     return {"message": "app is running"}
 
+
 @app.post("/predict")
 def predict(data: ChurnInput):
 
-    #JSON to DataFrame
-    pd = pd.DataFrame([data.dict()])
+    # JSON → DataFrame
+    df = pd.DataFrame([data.dict()])
 
-    #preprocessing
+    # preprocessing
     df = preprocess(df)
 
+    # ONLY THIS (important)
     df = df.reindex(columns=feature_columns, fill_value=0)
 
-    #feature alognment
-    df = df[feature_columns]
+    # scaling
+    df_scaled = scaler.transform(df)
 
-    #scaling
-    df = scaler.transform(df)
+    # prediction
+    prediction = model.predict(df_scaled)
+    proba = model.predict_proba(df_scaled)
 
-    #prediction
-    prediction = model.predict(df)
-    proba = model.predict_proba(df)
-
-    #final result
-    result = "Yes" if prediction[0] == 1 else "No"
+    result = "Yes (Churn)" if prediction[0] == 1 else "No (Not Churn)"
     confidence = float(max(proba[0]))
 
-
     return {
-        "churn_prediction" : int(prediction[0]),
+        "churn_prediction": int(prediction[0]),
         "result": result,
         "confidence": round(confidence, 4)
-
     }
--- api/preprocess.py
+++ api/preprocess.py
@@ -2,44 +2,26 @@ import pandas as pd
 
 def preprocess(df):
 
-   
-    # encoding for Binary input
-    
     df["gender"] = df["gender"].map({"Male": 1, "Female": 0})
     df["Partner"] = df["Partner"].map({"Yes": 1, "No": 0})
     df["Dependents"] = df["Dependents"].map({"Yes": 1, "No": 0})
     df["PhoneService"] = df["PhoneService"].map({"Yes": 1, "No": 0})
     df["PaperlessBilling"] = df["PaperlessBilling"].map({"Yes": 1, "No": 0})
 
-    
-    # Special case column
-  
-    df["MultipleLines"] = df["MultipleLines"].replace({
+    df["MultipleLines"] = df["MultipleLines"].map({
         "No phone service": 0,
         "No": 0,
         "Yes": 1
-    })
+    }).fillna(0)
 
-
-    # InternetService encoding
-  
     df["InternetService_Fiber optic"] = (df["InternetService"] == "Fiber optic").astype(int)
     df["InternetService_No"] = (df["InternetService"] == "No").astype(int)
 
-    
-    # PaymentMethod encoding
-    
     df["PaymentMethod_Electronic check"] = (df["PaymentMethod"] == "Electronic check").astype(int)
     df["PaymentMethod_Mailed check"] = (df["PaymentMethod"] == "Mailed check").astype(int)
     df["PaymentMethod_Credit card (automatic)"] = (df["PaymentMethod"] == "Credit card (automatic)").astype(int)
 
-   
-    # Drop original categorical columns
-   
     df = df.drop(["InternetService", "PaymentMethod"], axis=1)
-
-   
-    # Feature engineering
 
     df["Average"] = df["TotalCharges"] / (df["tenure"] + 1)
 
